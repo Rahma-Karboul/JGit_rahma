9dd5179d676daa840df57a36acafc2a9c79c3015
nasrimohtadi <mohtadi-nasri@forcus-corporation.com>
2020-12-01 12:09:43 +0100

    First commit

---
+++ src/main/java/com/focuscorp/DOFAN/DofanApplication.java
@@ -0,0 +1,13 @@
+package com.focuscorp.DOFAN;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DofanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DofanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/focuscorp/DOFAN/DofanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.focuscorp.DOFAN;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DofanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
