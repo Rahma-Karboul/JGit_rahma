31acee1f86830d960110f0d59aeb52bf875b066e
FOCUS <rahma.karboul@focus-corporation.com>
2020-12-11 16:17:08 +0100

    Add AddJob.html page and AddJobController

---
+++ src/main/java/com/focuscorp/DOFAN/controller/AddJobController.java
@@ -0,0 +1,13 @@
+package com.focuscorp.DOFAN.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class AddJobController {
+
+    @RequestMapping("/addJob")
+    public String addJob() {
+        return "addJob";
+    }
+}
