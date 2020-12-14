19b5d933f061861564b4df9a9287357c1b7fbcbb
nasrimohtadi <mohtadi-nasri@forcus-corporation.com>
2020-12-14 12:13:36 +0100

    Dofan Application deactivate spring security login page

--- src/main/java/com/focuscorp/DOFAN/DofanApplication.java
+++ src/main/java/com/focuscorp/DOFAN/DofanApplication.java
@@ -2,8 +2,9 @@ package com.focuscorp.DOFAN;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 public class DofanApplication {
 
 	public static void main(String[] args) {
