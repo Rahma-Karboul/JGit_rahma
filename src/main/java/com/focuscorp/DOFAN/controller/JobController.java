1b8e883063216db21c889e2627253c666f857fc6
FOCUS <rahma.karboul@focus-corporation.com>
2020-12-14 11:40:47 +0100

    Adding some changes

--- src/main/java/com/focuscorp/DOFAN/controller/JobController.java
+++ src/main/java/com/focuscorp/DOFAN/controller/JobController.java
@@ -1,10 +1,8 @@
 package com.focuscorp.DOFAN.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.util.Map;
 @Controller
 public class JobController {
 
