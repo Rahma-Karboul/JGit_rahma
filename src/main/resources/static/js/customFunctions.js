07c48dcec98074bb3a4162961d51fc2567e55066
TesnimK <tesnim.ksouri@focus-corporation.com>
2020-12-31 09:05:11 +0100

    build page "PIPELINE"

--- src/main/resources/static/js/customFunctions.js
+++ src/main/resources/static/js/customFunctions.js
@@ -85,3 +85,69 @@ for(var i in projects){
          $(".intercom-emoji-picker-emoji").show();
      }
  });
+
+ /* global flowjs */
+
+ window.addEventListener("load", func);
+
+ function func(){
+     console.log("LOADED");
+
+
+
+     var c = new flowjs.DiGraph();
+     c.addPaths([
+         ["Build", "local test"],
+         ["Build", "aa"],
+         ["local test", "Remote tests"],
+         ["aa", "Remote tests"],
+         ["Remote tests", "Quality check"],
+         [ "Quality check","Third party checks"],
+         ["Third party checks", "Artifact deployement"],
+         ["Artifact deployement","Production deployement"]
+     ]);
+
+
+
+
+
+
+     // Advanced Example with loading animation
+     var cf = new flowjs.DiFlowChart("canvas3", c);
+     cf.draw();
+     simuLoad(cf, c);
+ }
+
+
+ function simuLoad(flowChart, graph){
+     var walker = new flowjs.GraphWalker(graph);
+     walker.forEach(function(node){
+         var start = Math.random() * 1000 * 5;
+         var dur = Math.random() * 1000 * 5;
+         simulateLoading(node.id, start);
+         simulateDoneLoading(node.id, start + dur);
+     }, this);
+
+
+     function simulateLoading(itemId, timeout){
+         setTimeout(function(){
+             flowChart.updateItem(itemId, function(item){
+                 item.flowItem.toggleFlashing();
+             });
+         }, timeout);
+
+     }
+
+     function simulateDoneLoading(itemId, timeout){
+         setTimeout(function(){
+             flowChart.updateItem(itemId, function(item){
+                 item.flowItem.toggleFlashing();
+                 item.flowItem.color = "red";
+                 if (item.connectors === undefined){return;}
+                 item.connectors.forEach(function(conn){
+                     conn.color = "red";
+                 });
+             });
+         }, timeout);
+     }
+ }
